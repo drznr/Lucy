19c94e4328ebb58d847374a50c70ee4208893f86
drznr <44776027+drznr@users.noreply.github.com>
2020-03-19 19:25:22 +0200

    comments added

--- src/services/youtube-api.service.js
+++ src/services/youtube-api.service.js
@@ -12,6 +12,7 @@ async function youtubeQuery(query) {
         const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=` + query);
         return res.data;
     } catch (err) {
-        return err;
+        //////  HANDLE ERR
+        console.log(err);
     }
 }
