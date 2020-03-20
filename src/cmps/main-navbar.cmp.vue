08287e174cb3d741066ff55b2c14cc64301f1332
drznr <44776027+drznr@users.noreply.github.com>
2020-03-20 17:26:43 +0200

    staion-slider created

--- src/cmps/main-navbar.cmp.vue
+++ src/cmps/main-navbar.cmp.vue
@@ -1,22 +1,43 @@
 <template>
-    <nav class="main-nav">
-        <div class="nav-container container">
-            <router-link to="/" class="main-logo">Lucy</router-link>
-            <input type="checkbox" id="mobileNav" v-show="false" />
-            <label for="mobileNav" class="main-nav-mobile-btn">
-            <span>|</span>
-            </label>
-            <ul class="main-nav-nav"> 
-                <li><router-link to="/signup" class="main-nav-nav-link">Signup</router-link></li>
-                <li><button class="main-nav-nav-link btn-link">Login</button></li>
-                <li><router-link to="/station" class="main-nav-nav-link">Stations</router-link></li>
-            </ul>
-        </div>
-    </nav>
+  <nav class="main-nav" :class="{colored: isColored}">
+    <div class="nav-container container">
+      <router-link to="/" class="main-logo">Lucy</router-link>
+      <input type="checkbox" id="mobileNav" v-show="false" />
+      <label for="mobileNav" class="main-nav-mobile-btn">
+        <span>|</span>
+      </label>
+      <ul class="main-nav-nav">
+        <li>
+          <router-link to="/signup" class="main-nav-nav-link">Signup</router-link>
+        </li>
+        <li>
+          <button class="main-nav-nav-link btn-link">Login</button>
+        </li>
+        <li>
+          <router-link to="/station" class="main-nav-nav-link">Stations</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-   
+  data() {
+    return {
+        isColored: false
+    };
+  },
+  methods: {
+    animateNavbar(ev) {
+      this.isColored = window.scrollY > 100 ? true : false;
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.animateNavbar);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.animateNavbar);
+  }
 };
 </script>
---
+++ src/cmps/station-slider.cmp.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <swiper :options="swiperOptions" class="swiper container">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide>
+      <swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default {
+  name: 'swiper-example-loop',
+  title: 'Loop mode / Infinite loop',
+  data() {
+    return {
+      swiperOptions: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    };
+  },
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    //swiper: directive
+  }
+};
+</script>
