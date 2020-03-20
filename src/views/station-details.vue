b4cdc2b0743baff4b1d8ee7c8aae2c6400a1204b
RZalmon <ranzlmn@gmail.com>
2020-03-20 15:08:12 +0200

    youtube player added

--- src/views/station-details.vue
+++ src/views/station-details.vue
@@ -1,12 +1,42 @@
 <template>
-    <section class="station-details">
-        MUSIC PLAYER
-        <router-view></router-view>
-    </section>
+  <section class="station-details">
+    MUSIC PLAYER
+    <div class="player-container">
+      <youtube
+        :video-id="videoId"
+        ref="youtube"
+        :fitParent="true"
+        :player-vars="playerVars"
+        @playing="playing"
+      ></youtube>
+    </div>
+    <router-view></router-view>
+  </section>
 </template>
 
 <script>
 export default {
-    props: {}
+    props: {},
+  data() {
+    return {
+      videoId: "aYDfwUJzYQg",
+      playerVars: {
+        autoplay: 1
+      }
+    };
+  },
+  methods: {
+    // playVideo() {
+    //   this.player.playVideo()
+    // },
+    playing() {
+      console.log("o/ we are watching!!!");
+    }
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    }
+  }
 };
 </script>
