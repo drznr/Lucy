956b1c07d44d1068013d8980fae81c9a155544a3
Nadav Julius <nadavjulius@gmail.com>
2020-03-24 17:09:28 +0200

    all i wan tis pudding

--- src/cmps/station-filter.cmp.vue
+++ src/cmps/station-filter.cmp.vue
@@ -1,19 +1,26 @@
 <template>
 <section class="station-filter">
   <input type="text" placeholder="Search Startions!">
+  <select name="seachBy" id="">
+    <option value="all">All</option>
+    <option value="genres">Genres</option>
+    <option value="titles">Titles</option>
+  </select>
 </section>
 </template>
 
 <script>
 
 export default {
-  // data() {
- 
-  // },
+  data() {
+    return {
+      options: ['All', 'Genres', 'Title']
+    }
+  },
   // computed: {
   // }, 
   // methods: {
-  // },
+  // }, 
   // components: {
   // }
 };
